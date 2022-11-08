f7503f811d569eadd204b8eedd6e5a7ba1e72668
Syed <syedtathahiruddin@gmail.com>
2022-11-08 14:52:06 +0530

    added question number of leetcode

--- mergesortedarray.cpp
+++ mergesortedarray.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-
+//leetcode 88. Merge Sorted Array?
 void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
     int i = m-1,j = n-1;
      int k= m+n - 1;
