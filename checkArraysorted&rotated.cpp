a589fa8c9d6b292febf7636c29d64a7765fc27b4
Syed Tathahir Uddin <107764144+SyedTathahir@users.noreply.github.com>
2022-11-25 14:26:58 +0000

    completed the question

--- checkArraysorted&rotated.cpp
+++ checkArraysorted&rotated.cpp
@@ -1,1 +1,20 @@
 // leetcode : 1752. Check if Array Is Sorted and Rotated
+#include<iostream>
+using namespace std; 
+
+int main(){
+    
+        bool check(vector<int>& nums) {
+        int count = 0;
+        int n = nums.size();
+        
+        for(int i = 1 ;i < n ;i++){//linear traversing whole array
+            if(nums[i-1] > nums[i]){
+                count++;
+            }
+        }
+        if(nums[n-1]>nums[0])//cyclic traversing 
+            count++;
+        return count<=1;
+    }
+}
