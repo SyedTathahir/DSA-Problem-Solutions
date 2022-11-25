433870a9402cabc016cbe0ad3ca1b7aca1f768fd
Syed Tathahir Uddin <107764144+SyedTathahir@users.noreply.github.com>
2022-11-25 14:08:02 +0000

    completed the problem !!

---
+++ rotateArray.cpp
@@ -0,0 +1,14 @@
+//189. Rotate Array
+#include <iostream>
+using namespace std;
+
+int main (){
+        void rotate(vector<int>& nums, int k) {
+        vector<int> temp(nums.size());
+        for(int i = 0 ;i<nums.size();i++){
+            temp[(i + k) % nums.size()]= nums[i];
+        }
+        //copy temp into num vector!
+        nums = temp;
+    }
+}
